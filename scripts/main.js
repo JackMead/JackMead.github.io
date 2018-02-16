959b34907838a67c232fe6eec11b0113012bbb0d
JackMead <Jack.Mead@softwire.com>
2018-02-16 16:00:40 +0000

    Improved naming, removed test files

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-    var numberOfPixels = 400;
-
-    for (var i = 0; i < numberOfPixels; i++) {
+    var pixelsPerRow = 15;
+    var numberRows = 15;
+    var numberPixels = pixelsPerRow * numberRows;
+    
+    for (var i = 0; i < numberPixels; i++) {
         var pixelId = "pixelCheckbox" + i;
         $("#paint").append("<input type=\"checkbox\" class=\"pixelCheckbox\" id=\""+pixelId+"\"></input><label class=\"pixel\" for=\""+pixelId+"\"></label>");
     }
