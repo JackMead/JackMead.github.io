e3de2b85797eec9b019c714296a12ddfee485777
JackMead <Jack.Mead@softwire.com>
2018-03-09 14:39:59 +0000

    Added giphy page

--- scripts/giphy.js
+++ scripts/giphy.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
-    fetch("http://api.giphy.com/v1/gifs/trending?api_key=cIjd21iM1leLJ1bW1ZAEcsEfVWnW6H5L")
+    fetch("http://api.giphy.com/v1/gifs/random?api_key=cIjd21iM1leLJ1bW1ZAEcsEfVWnW6H5L")
         .then((response) => {
-            console.log(response.status);
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new error("Problem loading gif");
+            }
+        })
+        .then(response => {
+            $("#img").attr("src", response.data.image_url);
+        })
+        .catch(e => {
+            console.log(e);
         });
 });
